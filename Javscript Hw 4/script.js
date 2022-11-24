26f826800b5535e60dc38b9bba2d062aa6732fba
Asimowsky <kebirasim@hotmail.com>
2022-11-24 15:50:40 +0100

    update for first exercise

--- Javscript Hw 4/script.js
+++ Javscript Hw 4/script.js
@@ -1,17 +1,31 @@
 //as i understood we need to merge the arrays below without kate and jill
+// var JohnFollowers = ["Bill", "Kate", "Jill", "Tom", "Mary", "Mark"];
+// var JaneFollowers = ["Will", "Kate", "Jill", "Kyle", "Toby", "Nicolle"];
+
+// var mergeFollowers = JohnFollowers.concat(JaneFollowers);
+// for (var i = 0; i < mergeFollowers.length; i++) {
+//     if (mergeFollowers[i] == "Kate") {
+//         mergeFollowers.splice([i], 1);
+//     }
+//     if (mergeFollowers[i] == "Jill") {
+//         mergeFollowers.splice([i], 1);
+//     }
+// }
+
+//only kate and jill
 var JohnFollowers = ["Bill", "Kate", "Jill", "Tom", "Mary", "Mark"];
 var JaneFollowers = ["Will", "Kate", "Jill", "Kyle", "Toby", "Nicolle"];
 
-var mergeFollowers = JohnFollowers.concat(JaneFollowers);
-for (var i = 0; i < mergeFollowers.length; i++) {
-    if (mergeFollowers[i] == "Kate") {
-        mergeFollowers.splice([i], 1);
+var mergeFollowers = [];
+for (var i = 0; i < JohnFollowers.length; i++) {
+   for(var j = 0; j<JaneFollowers.length;j++){
+    if(JohnFollowers[i]== JaneFollowers[j]){
+        mergeFollowers.push(JohnFollowers[i]);
+        mergeFollowers.push(JaneFollowers[j]); //maybe
     }
-    if (mergeFollowers[i] == "Jill") {
-        mergeFollowers.splice([i], 1);
-    }
+   }
 }
-console.log(mergeFollowers);
+console.log(mergeFollowers)
 
 //super mario tower
 var superMario = [];
@@ -23,13 +37,13 @@ for (var i = 0; i < 6; i++) {
 //problem was solved only with console.log(superMario) but decided to research how to take out to hell that brackets and commas so pozdrav.
 
 //multiplication table with nested loop
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var result = 0;
-for (var i = 0; i < numbers.length; i++) {
-    for (var j = 0; j < numbers.length; j++) {
-        result = numbers[i] * numbers[j];
-        console.log(`${numbers[i]} * ${numbers[j]} = ${result}`);
-    }
-}
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// var result = 0;
+// for (var i = 0; i < numbers.length; i++) {
+//     for (var j = 0; j < numbers.length; j++) {
+//         result = numbers[i] * numbers[j];
+//         console.log(`${numbers[i]} * ${numbers[j]} = ${result}`);
+//     }
+// }
 
 
