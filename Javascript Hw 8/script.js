47f4fc2125fe656521710f857aa77abfb27c367a
Asimowsky <kebirasim@hotmail.com>
2022-12-05 14:55:40 +0100

    hw8

--- Javascript Hw 8/script.js
+++ Javascript Hw 8/script.js
@@ -1,100 +1,43 @@
-// let box = document.querySelector(".box");
-// let increase = document.getElementById("increase");
-// let decrease = document.getElementById("decrease");
 
-// increase.addEventListener("click", function() {
-//     localStorage.setItem("changeBox", "increase");
-//     let type = localStorage.getItem("changeBox");
-//     HandleBoxChange(type);
-// });
-
-// decrease.addEventListener("click", function() {
-//     localStorage.setItem("changeBox", "decrease");
-//     let type = localStorage.getItem("changeBox");
-//     HandleBoxChange(type);
-// });
-
-// const HandleBoxChange = (type) => {
-//     if(type === "increase") {
-//         box.classList.add("boxed-increased");
-//     } else {
-//         box.classList.remove("boxed-increased");
-//     }
-// }
-
-// let type = localStorage.getItem("changeBox");
-// HandleBoxChange(type);
-
-
-// JSON -> Javascript Object Notation
-
-// API -> Application Programming Interface
-
-let person = `{
-    "name": "Mario Petkovski",
-    "location": "Aerdrom, Skopje, Macedonia",
-    "age": 29,
-    "hobbies": ["javascript", "film", "photography"],
-    "address": {
-        "street": "Bul. Pero Perovski 1000",
-        "appartment": 14
-    },
-    "has_drivers_license": true
-}`;
-
-console.log(   person.location  );
-
-// Promises
-
-const promise = new Promise((resolve, reject) => {
-    let result = 5;
-    if(2 + 3 === result) {
-        resolve("Promise RESOLVED");
-    } else {
-        reject("Promise REJECTED");
-    }
-});
-
-promise.then(res => {
-    console.log(res);
-}).catch(err => {
-    console.log(err);
-});
-
-const HasVacancy = new Promise((resolve, reject) => {
-    let room_is_available = true;
-
-    if(room_is_available) {
-        setTimeout(() => {
-            resolve("RESOLVED");
-        }, 2000);
-    } else {
-        setTimeout(() => {
-            reject("REJECTED");
-        }, 2000);
-    }
-});
-
-
-HasVacancy.then(res => {
-    console.log(res);
-}).catch(err => {
-    alert(err);
-});
-
-
-// Fetch
-
-fetch("https://www.boredapi.com/api/activity")
+fetch("https://jsonplaceholder.typicode.com/todos/")
     .then(res => {
         console.log(res);
-        if(res.ok) {
+        if (res.ok) {
             return res.json();
         } else {
-            console.error("Ne ti e okej API-to");
+            console.error("Check your connection");
         }
-    }).then(kako_sakas_moze => {
-        console.log(kako_sakas_moze);
+    }).then(fakeapi => {
+        const ul_list = document.createElement('ul');
+        const bodyUl = document.getElementById("test")
+        for(let i=1; i<=100;i++){
+        const list_item = document.createElement('li');
+        const titles = (fakeapi[i].title)
+        list_item.innerHTML = titles;
+        ul_list.appendChild(list_item);
+    
+     }
+     
+     bodyUl.append(ul_list);
     }).catch(err => {
         console.log(err);
     });
+
+fetch("https://jsonplaceholder.typicode.com/photos/21")
+    .then(res => {
+        console.log(res);
+        if (res.ok) {
+            return res.json();
+        } else {
+            console.error("Check your connection");
+        }
+    }).then(fakeapi_photos => {
+        const fetchImg = document.getElementById("img-left");
+        fetchImg.src = fakeapi_photos.url;
+        fetchImg.style.width = "200px";
+
+    }).catch(err => {
+        console.log(err);
+    });
+
+
